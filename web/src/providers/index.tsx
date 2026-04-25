d023c7d80cad57dd0bc0373fed1e23e5a78e8eea
Alexander Olsen <aolsenjazz@gmail.com>
2026-04-25 10:30:51 -0400

    Add default shadcn components

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -1,7 +1,12 @@
 import { ReactNode } from 'react';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 import { QueryClientProvider } from './QueryClientProvider';
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+    </QueryClientProvider>
+  );
 }
